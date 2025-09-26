b3fe04afb7f4fbb248e26f1a4fe91fdee650d69e
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-09-26 22:05:10 +0600

    add: string reverse with loop added

--- prob1/main.go
+++ prob1/main.go
@@ -2,12 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
+func pattern() {
 	for row := 1; row <= 5; row++ {
 		for col := 1; col <= row; col++ {
 			fmt.Print("*")
 		}
 		fmt.Println()
 	}
+}
+
+func reverse() {
+	s := "hello"
+	rev := ""
+
+	for i := len(s) - 1; i >= 0; i-- {
+		rev += string(s[i])
+	}
+	fmt.Println(rev)
+}
+
+func main() {
+	pattern()
+	reverse()
 
 }
