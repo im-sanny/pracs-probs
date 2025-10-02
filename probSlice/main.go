e4b72d42092afea0e8dc207a68cb07dfc7b9b721
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-10-02 16:07:27 +0600

    edit&add: edited some mistake and added notes for better understanding of some problem

--- probSlice/main.go
+++ probSlice/main.go
@@ -2,50 +2,56 @@ package main
 
 import "fmt"
 
-// func changeSlice(p []int) []int {
-// 	p[0] = 10         // [10, 6, 7] overwrite 5 from x
-// 	p = append(p, 11) // [10, 6, 7, 11] new array creation -> disconnect y from  a and x
-// 	return p          // return for y
-// }
+func changeSlice(p []int) []int {
+	p[0] = 10         // [10, 6, 7] overwrite 5 from x
+	p = append(p, 11) // [10, 6, 7, 11] new array creation -> disconnect y from  a and x
+	return p          // return for y
+}
 
-func main() {
-	// x := []int{1, 2, 3, 4, 5}
-	// x = append(x, 6) // [1, 2, 3, 4, 5, 6]
-	// x = append(x, 7) // [1, 2, 3, 4, 5, 6, 7]
+func prob1() {
+	x := []int{1, 2, 3, 4, 5}
+	x = append(x, 6) // [1, 2, 3, 4, 5, 6]
+	x = append(x, 7) // [1, 2, 3, 4, 5, 6, 7]
 
-	// a := x[4:]          // [5, 6, 7]
-	// y := changeSlice(a) //sending a in changeSlice. got [10, 6, 7, 11] from changeSlice
+	a := x[4:]          // [5, 6, 7]
+	y := changeSlice(a) //sending a in changeSlice. got [10, 6, 7, 11] from changeSlice
 
-	// fmt.Println(x) //[1, 2, 3, 4, 10, 6, 7]
-	// fmt.Println(y) //[10, 6, 7, 11]
+	fmt.Println(x) //[1, 2, 3, 4, 10, 6, 7]
+	fmt.Println(y) //[10, 6, 7, 11]
+}
 
-	// a := []int{1, 2, 3}
-	// b := a //[1, 2, 3]
-	// a = append(a, 4) // [1, 2, 3, 4]
-	// b[0] = 99 // [99, 2, 3]
-	// fmt.Println(a)
-	// fmt.Println(b)
+func prob2() {
+	a := []int{1, 2, 3}
+	b := a           // [1, 2, 3]
+	a = append(a, 4) // [1, 2, 3, 4]
+	b[0] = 99        // [99, 2, 3]
+	fmt.Println(a)   // [1, 2, 3, 4]
+	fmt.Println(b)   // [99, 2, 3]
+}
 
-	x := make([]int, 2, 3)
-	x[0], x[1] = 1, 2 // [1, 2]
-	y := x            // [1, 2]
-	x = append(x, 3)  // [1, 2, 3]
-	y = append(y, 4)  // [1, 2, 4] // after allocation y gets disconnected from x
-	x[0] = 99         //[99, 2, 4]
-	fmt.Println(x)    //[99, 2, 4]
-	fmt.Println(y)    // [1, 2, 4]
+func prob3() {
+	x := make([]int, 2, 3) //len = 2, cap = 3
+	x[0], x[1] = 1, 2      // [1, 2]
+	y := x                 // [1, 2]
+	x = append(x, 3)       // [1, 2, 3] len= 3, cap = 3
+	y = append(y, 4)       // [1, 2, 4] len= 3 cap = 3, here 4 will overwrite 3, bc y had 2 len and 3 cap so it won't create new array and that's why it will change the original array.
+	x[0] = 99              // [99, 2, 4] // since there's no new array and no disconnection so y will get this change of x.
+	fmt.Println(x)         // [99, 2, 4]
+	fmt.Println(y)         // [99, 2, 4]
+}
 
+func prob4() {
 	p := []int{1, 2}
-	q := p
+	q := p           // [1, 2]
 	p = append(p, 3) //[1, 2, 3]
-	q = append(q, 4) //[1, 2, 3,4]
-	p[1] = 88        //[2,88, 3]
-	q[0] = 77        //[77, 2, 3, 4]
-	fmt.Println(p)   //
-	fmt.Println(q)   //
+	q = append(q, 4) //[1, 2, 4] // After p = append(p, 3), p and q point to completely separate arrays. They are no longer connected.
+	p[1] = 88        //[1, 88, 3]
+	q[0] = 77        //[77, 2, 4]
+	fmt.Println(p)   //[1, 88, 3]
+	fmt.Println(q)   //[77, 2, 4]
+}
 
-	// I guess my mistake in these problem is I'm not calculating len and cap
-
+func prob5() {
 	m := []int{1, 2, 3, 4}
 	n := m[:2]       // [1, 2]
 	m = append(m, 5) //[1, 2, 3, 4, 5]
@@ -53,7 +59,9 @@ func main() {
 	m[0] = 99        //[99, 2, 3, 4, 5]
 	fmt.Println(m)   //[99, 2, 3, 4, 5]
 	fmt.Println(n)   //[1, 2, 6]
+}
 
+func prob6() {
 	s1 := []int{1, 2}
 	s2 := s1
 	s1 = append(s1, 3) // [1,2,3]
@@ -62,5 +70,13 @@ func main() {
 	s2[0] = 99         // [99, 2, 4]
 	fmt.Println(s1)    // [1,2,3,5]
 	fmt.Println(s2)    // [99, 2, 4]
+}
 
+func main() {
+	prob1()
+	prob2()
+	prob3()
+	prob4()
+	prob5()
+	prob6()
 }
